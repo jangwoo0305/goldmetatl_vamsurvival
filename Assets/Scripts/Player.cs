e591e54bd609beb908f6c1e5b57867fcbdba0b29
jangwoo0305 <tjwkddn970305@gmail.com>
2025-09-04 21:57:34 +0900

    make animator, animator override

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,15 @@ public class Player : MonoBehaviour
     public Vector2 inputVec;
     public float speed;
     Rigidbody2D rigid;
+    SpriteRenderer spriter; 
+    Animator anim;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         rigid =  GetComponent<Rigidbody2D>();
+        spriter =  GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>(); 
     }
 
     void FixedUpdate()
@@ -28,5 +32,16 @@ public class Player : MonoBehaviour
     void OnMove(InputValue value)
     {
         inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate() //업데이트가 끝나고 다음 프레임으로 넘어가기 전 실행되는 업데이트 함수
+    
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+            
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
